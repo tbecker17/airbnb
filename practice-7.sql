c5a86c91a93a3a5030ddb778a5dcb55cc76493cd
Tim Becker <tim.becker@kellogg.northwestern.edu>
2023-04-11 20:37:21 +0000

    complete with joining

--- practice-7.sql
+++ practice-7.sql
@@ -1,5 +1,8 @@
 -- What is the number of reviews and date of the latest review, by property type?
-
+SELECT property_type, COUNT(*) AS review_count, MAX(date_reviewed) AS latest_review_date
+FROM listings
+JOIN reviews ON listings.id = reviews.listing_id
+GROUP BY property_type;
 -- +-------------------------------------+----------+----------------------------+
 -- | Boat                                | 2        | 2021-06-19                 |
 -- | Entire bungalow                     | 424      | 2021-10-18                 |
